e115b0670973aceb8c28ea076be476f2dbf4d2d7
Andreas Fuchs <asf@boinkor.net>
2018-05-20 20:39:06 +0200

    Correctly handle the mash macro: no shift!

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -24,50 +24,46 @@
 // Support for host power management (suspend & wakeup)
 #include "Kaleidoscope-HostPowerManagement.h"
 
-#define MACRO_LEFTPAREN 3
-#define MACRO_RIGHTPAREN 4
-#define MACRO_ALLMODS 5
-#define Macro_RightParen M(MACRO_RIGHTPAREN)
-#define Macro_LeftParen M(MACRO_LEFTPAREN)
-#define Macro_AllMods M(MACRO_ALLMODS)
+// Keymaps:
+enum {QWERTY, FUNCTION};
 
-#define GENERIC_FN2  KEYMAP_STACKED ( \
-    ___,                  Key_F1,                  Key_F2,                   Key_F3,                   Key_F4,                     Key_F5,                 Macro_AllMods, \
-    Key_Tab,              ___,                     Key_mouseScrollUp,        ___,                      Key_mouseBtnR,              XXX,                    XXX, \
-    Key_Home,             XXX,                     Key_mouseScrollDn,        XXX,                      Key_mouseBtnL,              XXX, \
-    Key_LeftShift,        Key_Home,                Key_End,                  XXX,                      XXX,                        XXX,                    Key_LeftAlt, \
-    Key_LeftShift,        Key_Backspace,           Key_LeftGui,              Key_LeftControl, \
-    ___,                                        \
-                                                \
-    Macro_AllMods,        Key_F6,                  Key_F7,                   Key_F8,                   Key_F9,                     Key_F10,                Key_Pipe, \
-    Key_LeftCurlyBracket, Key_RightCurlyBracket,   Macro_LeftParen,          Macro_RightParen,         Key_LeftBracket,            Key_RightBracket,       Key_F12, \
-    Key_LeftArrow,        Key_DownArrow,           Key_UpArrow,              Key_RightArrow,           Consumer_ScanPreviousTrack, Consumer_ScanNextTrack, \
-    Key_RightAlt,         Consumer_PlaySlashPause, Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,                        Key_Backslash,          Key_RightShift, \
-    Key_RightControl,     Key_RightGui,            Key_Spacebar,             Key_RightShift, \
-    ___                                         \
-)
+// Macros:
+enum { MACRO_LEFTPAREN, MACRO_RIGHTPAREN, MACRO_ALLMODS };
 
+// *INDENT-OFF*
+KEYMAPS(
+  [QWERTY] = KEYMAP_STACKED
+    (___,                      Key_1,         Key_2,        Key_3,           Key_4,         Key_5,     M(MACRO_ALLMODS),
+     Key_Backtick,             Key_Q,         Key_W,        Key_E,           Key_R,         Key_T,     Key_Tab,
+     Key_Escape,               Key_A,         Key_S,        Key_D,           Key_F,         Key_G,
+     Key_LeftShift,            Key_Z,         Key_X,        Key_C,           Key_V,         Key_B,     Key_LeftAlt,
+     Key_LeftShift,            Key_Backspace, Key_LeftGui,  Key_LeftControl,
+     ShiftToLayer(FUNCTION),
 
-#define QWERTY KEYMAP_STACKED (                 \
-     ___,                      Key_1,         Key_2,        Key_3,           Key_4,         Key_5,     Macro_AllMods, \
-     Key_Backtick,             Key_Q,         Key_W,        Key_E,           Key_R,         Key_T,     Key_Tab, \
-     Key_Escape,               Key_A,         Key_S,        Key_D,           Key_F,         Key_G, \
-     Key_LeftShift,            Key_Z,         Key_X,        Key_C,           Key_V,         Key_B,     Key_LeftAlt, \
-     Key_LeftShift,            Key_Backspace, Key_LeftGui,  Key_LeftControl, \
-     Key_KeymapNext_Momentary,                  \
-                                                \
-     Macro_AllMods,            Key_6,         Key_7,        Key_8,           Key_9,         Key_0,     Key_Minus, \
-     Key_Enter,                Key_Y,         Key_U,        Key_I,           Key_O,         Key_P,     Key_Equals, \
-     Key_H,                    Key_J,         Key_K,        Key_L,           Key_Semicolon, Key_Quote, \
-     Key_RightAlt,             Key_N,         Key_M,        Key_Comma,       Key_Period,    Key_Slash, Key_RightShift, \
-     Key_RightControl,         Key_RightGui,  Key_Spacebar, Key_RightShift, \
-     Key_KeymapNext_Momentary                   \
+     M(MACRO_ALLMODS), Key_6,        Key_7,        Key_8,          Key_9,         Key_0,     Key_Minus,
+     Key_Enter,        Key_Y,        Key_U,        Key_I,          Key_O,         Key_P,     Key_Equals,
+     Key_H,            Key_J,        Key_K,        Key_L,          Key_Semicolon, Key_Quote,
+     Key_RightAlt,     Key_N,        Key_M,        Key_Comma,      Key_Period,    Key_Slash, Key_RightShift,
+     Key_RightControl, Key_RightGui, Key_Spacebar, Key_RightShift,
+     ShiftToLayer(FUNCTION)),
+
+  [FUNCTION] = KEYMAP_STACKED
+    (___,                  Key_F1,                  Key_F2,                   Key_F3,                   Key_F4,                     Key_F5,                 M(MACRO_ALLMODS),
+     Key_Tab,              ___,                     Key_mouseScrollUp,        ___,                      Key_mouseBtnR,              XXX,                    XXX,
+     Key_Home,             XXX,                     Key_mouseScrollDn,        XXX,                      Key_mouseBtnL,              XXX,
+     Key_LeftShift,        Key_Home,                Key_End,                  XXX,                      XXX,                        XXX,                    Key_LeftAlt,
+     Key_LeftShift,        Key_Backspace,           Key_LeftGui,              Key_LeftControl,
+     ___,
+
+     M(MACRO_ALLMODS),     Key_F6,                  Key_F7,                   Key_F8,                   Key_F9,                     Key_F10,                Key_Pipe,
+     Key_LeftCurlyBracket, Key_RightCurlyBracket,   M(MACRO_LEFTPAREN),       M(MACRO_RIGHTPAREN),      Key_LeftBracket,            Key_RightBracket,       Key_F12,
+     Key_LeftArrow,        Key_DownArrow,           Key_UpArrow,              Key_RightArrow,           Consumer_ScanPreviousTrack, Consumer_ScanNextTrack,
+     Key_RightAlt,         Consumer_PlaySlashPause, Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,                        Key_Backslash,          Key_RightShift,
+     Key_RightControl,     Key_RightGui,            Key_Spacebar,             Key_RightShift,
+     ___
+       )
       )
-
-const Key keymaps[][ROWS][COLS] PROGMEM = {
-  QWERTY,
-  GENERIC_FN2,
-};
+// *INDENT-ON*
 
 static kaleidoscope::LEDSolidColor solidRed(160, 0, 0);
 static kaleidoscope::LEDSolidColor solidOrange(140, 70, 0);
@@ -96,8 +92,11 @@ void toggleLedsOnSuspendResume(kaleidoscope::HostPowerManagement::Event event) {
   }
 }
 
+void hostPowerManagementEventHandler(kaleidoscope::HostPowerManagement::Event event) {
+  toggleLedsOnSuspendResume(event);
+}
+
 static void OneShotHyper(uint8_t keyState) {
-  handleKeyswitchEvent(OSM(LeftShift), UNKNOWN_KEYSWITCH_LOCATION, keyState);
   handleKeyswitchEvent(OSM(LeftControl), UNKNOWN_KEYSWITCH_LOCATION, keyState);
   handleKeyswitchEvent(OSM(LeftAlt), UNKNOWN_KEYSWITCH_LOCATION, keyState);
   handleKeyswitchEvent(OSM(LeftGui), UNKNOWN_KEYSWITCH_LOCATION, keyState);
@@ -115,7 +114,7 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
 }
 
 void setup() {
-  BootKeyboard.begin();
+  Kaleidoscope.setup();
   Kaleidoscope.use(&LEDControl,
                    &LEDOff,
                    &OneShot,
@@ -128,7 +127,6 @@ void setup() {
   AlphaSquare.color = { 255, 0, 0 };
   HostPowerManagement.enableWakeup();
   LEDOff.activate();
-  Kaleidoscope.setup();
 }
 
 
